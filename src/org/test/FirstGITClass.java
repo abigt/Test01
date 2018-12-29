6dd7719e9ed1772658ae9f7fdcadaeb3e008b4b2
abigt <scooby.abi@gmail.com>
2018-12-29 19:36:46 +0530

    First Change

--- src/org/test/FirstGITClass.java
+++ src/org/test/FirstGITClass.java
@@ -5,6 +5,7 @@ public class FirstGITClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First GIT Project");
+		System.out.println("Changes first time");
 	}
 
 }
